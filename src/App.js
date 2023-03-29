b928f7de983c7c481dbebac92b31453177a39acb
osboxes.org <milktealliance@protonmail.com>
2023-03-29 22:42:40 +0100

    add calendar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js'
 import Search from './components/Search';
+import "flatpickr/dist/themes/material_green.css";
+import Flatpickr from "react-flatpickr";
+import { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div>
-     <Search/>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      date: new Date()
+    };
+  }
+
+  render() {
+    const { date } = this.state;
+    return (
+      <div>
+        <Flatpickr
+          value={date}
+          onChange={([date]) => {
+            this.setState({ date });
+          }}
+        />
+        <Search /> {/* Use the Search component */}
+      </div>
+    );
+  }
 }
 
 export default App;
