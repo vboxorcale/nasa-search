c9fc0d3f51d0578c2463b854b980a7b35ec42377
osboxes.org <milktealliance@protonmail.com>
2023-03-30 21:10:36 +0100

    change css of datepicker.css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () =>{
     // A Bootstrap Navbar with a light background
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       {/* //// A Navbar brand with the text "Bootcamp   */}
-      <div className="brandtext"><a className="navbar-brand" href="#">Bootcamp</a></div>
+      <div className="brandtext"><a className="navbar-brand" href="/" aria-disabled="true">Bootcamp</a></div>
       {/* // A button to toggle the Navbar collapse */}
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
@@ -15,13 +15,13 @@ const Navbar = () =>{
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item active">
-            <a className="nav-link" href="#">Front-end Development <span className="sr-only">(current)</span></a>
+            <a className="nav-link"  href="/" aria-disabled="true">Front-end Development <span className="sr-only">(current)</span></a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Features</a>
+            <a className="nav-link"  href="/" aria-disabled="true">Features</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Contact</a>
+            <a className="nav-link"  href="/" aria-disabled="true">Contact</a>
           </li>
         </ul>
       </div>
